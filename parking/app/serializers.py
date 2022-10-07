3ebea52196d30c809526ae6aed113b9b36b0ead1
anacarneirosv <ana.carneiro@convatec.com>
2022-10-07 19:00:20 +0100

    Added validate super user

--- parking/app/serializers.py
+++ parking/app/serializers.py
@@ -2,7 +2,8 @@ from dataclasses import fields
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from parking.app.models import Damage, ParkingMeter
+from app.models import ParkingMeter, Damage
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,6 +20,22 @@ class UserSerializer(serializers.ModelSerializer):
         if 'password' in validated_data:
             instance.password = make_password(validated_data['password'], validated_data['username'])
         
+        user =  self.context['request'].user
+        errors = []
+        if user.is_superuser:
+            is_superuser = validated_data.get('is_superuser', instance.is_superuser)
+            if not is_superuser and instance.is_superuser:
+                total_superuser = User.objects.filter(is_superuser = True).count()
+                if total_superuser > 1:
+                    instance.is_superuser = is_superuser
+                else:
+                    errors.append({"is_superuser": "You cant't not change the last super user"})
+            else:
+                instance.is_superuser = is_superuser
+
+            if errors:
+                raise serializers.ValidationError(errors)    
+
         instance.is_active = validated_data.get('is_active', instance.is_active)
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
