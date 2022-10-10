166ee4250248cf3ba4223ae4b852363652258700
anacarneirosv <ana.carneiro@convatec.com>
2022-10-10 12:12:09 +0100

    Fix models

--- parking/app/models.py
+++ parking/app/models.py
@@ -8,8 +8,8 @@ class ParkingMeter(models.Model):
     lat = models.DecimalField(max_digits=22, decimal_places=16, null=False)
 
 class Damage(models.Model):
-    parking_meter = models.ForeignKey(ParkingMeter, on_delete=models.CASCADE)
+    parking_meter = models.ForeignKey(ParkingMeter, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(default=timezone.now)    
     fixed = models.BooleanField()
     description = models.CharField(max_length=280)
-    assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,  null=True)
+    assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
